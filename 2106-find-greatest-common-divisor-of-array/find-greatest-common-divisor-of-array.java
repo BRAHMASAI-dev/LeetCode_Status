134878a67e5fc25132513c8f986d06dbaca84865
Brahma sai <brahmasai822@gmail.com>
2026-08-05 15:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (56.46%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -4,18 +4,15 @@ class Solution {
         int n2 = 0;
         int gcd = 1;
         for(int i=0; i<nums.length; i++){
-            if(nums[i]<n1){
+            if(nums[i]<n1)
                 n1 = nums[i];
-            }
-            if(nums[i]>n2){
+            if(nums[i]>n2)
                 n2 = nums[i];
-            }
         }
-        for(int i=2; i<=n2; i++){
-            if(n1%i == 0 && n2%i == 0){
-                if(gcd<i)
-                    gcd = i;
-            }
+        for(int i=n1; i>1; i--){
+            if(n1%i == 0)
+                if(n2%i == 0)
+                    return i;
         }
         return gcd;
     }
