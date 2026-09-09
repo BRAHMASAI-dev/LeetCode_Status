ced0a2826729f2d6d66cdc9235f1f1921641bda9
Brahma sai <brahmasai822@gmail.com>
2026-09-09 18:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (52.27%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean ismirror(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val!=q.val) return false;
+        boolean l = ismirror(p.left, q.right);
+        boolean r = ismirror(p.right, q.left);
+        return l && r;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return ismirror(root.left,root.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
